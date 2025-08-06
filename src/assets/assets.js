85e4f2ca4b6cc296481ec35ed3d602d3f2b6aeec
Salman Lania <78688636+salmanlania@users.noreply.github.com>
2025-08-06 13:04:29 -0700

    he

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -122,7 +122,7 @@ export const products = [
     },
     {
         _id: "aaaac",
-        name: "Girls Round Neck Cotton Top",
+        name: "Girls Round Neck Cotton Topper",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 220,
         image: [p_img3],
