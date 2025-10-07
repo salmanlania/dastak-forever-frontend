6829a5cc6f03c42100e1640cc57a2e49ec0836fd
Salman <salmanlania@gmail.com>
2025-10-07 13:10:48 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,24 +18,24 @@ import ComingSoon from './tempComponent/ComingSoon'
 
 const App = () => {
   return (
-    // <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-    //   <ToastContainer />
-    //   <Navbar />
-    //   <SearchBar />
-    //   <Routes>
-    //     <Route path='/' element={<Home />} />
-    //     <Route path='/collection' element={<Collection />} />
-    //     <Route path='/about' element={<About />} />
-    //     <Route path='/contact' element={<Contact />} />
-    //     <Route path='/product/:productId' element={<Product />} />
-    //     <Route path='/cart' element={<Cart />} />
-    //     {/* <Route path='/login' element={<Login />} /> */}
-    //     <Route path='/place-order' element={<PlaceOrder />} />
-    //     <Route path='/orders' element={<Orders />} />
-    //   </Routes>
-    //   <Footer />
-    // </div>
-    <ComingSoon />
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        {/* <Route path='/login' element={<Login />} /> */}
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
+      </Routes>
+      <Footer />
+    </div>
+    // <ComingSoon />
   )
 }
 
