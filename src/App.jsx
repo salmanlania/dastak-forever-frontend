06e894d3a83cfe6f03f375adde8cf312d6a3535b
Salman <salmanlania@gmail.com>
2025-10-07 14:38:43 +0500

    more

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-// import React from 'react'
-// import Navbar from './components/Navbar'
-// import { Route, Routes } from 'react-router-dom'
-// import Home from './pages/Home'
-// import Collection from './pages/Collection'
-// import About from './pages/About'
-// import Contact from './pages/Contact'
-// import Cart from './pages/Cart'
-// import Product from './pages/Product'
-// import Footer from './components/Footer'
-// import Login from './pages/Login'
-// import PlaceOrder from './pages/PlaceOrder'
-// import Orders from './pages/Orders'
-// import SearchBar from './components/SearchBar'
-// import { ToastContainer } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Footer from './components/Footer'
+import Login from './pages/Login'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import SearchBar from './components/SearchBar'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import ComingSoon from './tempComponent/ComingSoon'
 
 const App = () => {
