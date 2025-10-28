08479e12dea5668d8d8195ccfb58a4ca76c39333
Salman Lania <78688636+salmanlania@users.noreply.github.com>
2025-10-28 13:10:06 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,32 +17,31 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PrivacyPolicy from './pages/PrivacyPolicy'
 import DeliveryPolicy from './pages/DeliveryPolicy'
-import ComingSoon from './tempComponent/ComingSoon'
+// import ComingSoon from './tempComponent/ComingSoon'
 
 const App = () => {
   return (
-    // <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-    //   <ToastContainer />
-    //   <Navbar />
-    //   <SearchBar />
-    //   <Routes>
-    //     <Route path='/' element={<Home />} />
-    //     <Route path='/main' element={<Home />} />
-    //     <Route path='/product' element={<Collection />} />
-    //     <Route path='/about' element={<About />} />
-    //     <Route path='/contact' element={<Contact />} />
-    //     <Route path='/product/:productId' element={<Product />} />
-    //     <Route path='/cart' element={<Cart />} />
-    //     <Route path='/return-policy' element={<ReturnPolicy />} />
-    //     <Route path='/privacy-policy' element={<PrivacyPolicy />} />
-    //     <Route path='/delivery' element={<DeliveryPolicy />} />
-    //     {/* <Route path='/login' element={<Login />} /> */}
-    //     <Route path='/place-order' element={<PlaceOrder />} />
-    //     <Route path='/orders' element={<Orders />} />
-    //   </Routes>
-    //   <Footer />
-    // </div>
-    <ComingSoon />
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <ToastContainer />
+      <Navbar />
+      <SearchBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/product' element={<Collection />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/product/:productId' element={<Product />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/return-policy' element={<ReturnPolicy />} />
+        <Route path='/privacy-policy' element={<PrivacyPolicy />} />
+        <Route path='/delivery' element={<DeliveryPolicy />} />
+        {/* <Route path='/login' element={<Login />} /> */}
+        <Route path='/place-order' element={<PlaceOrder />} />
+        <Route path='/orders' element={<Orders />} />
+      </Routes>
+      <Footer />
+    </div>
+    // <ComingSoon />
   )
 }
 
