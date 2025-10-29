7186feb66c8141fff7899b7fc24ff1b1ae1455ff
Salman <salmanlania@gmail.com>
2025-10-29 17:08:39 +0500

    Update ComingSoon.jsx

--- src/tempComponent/ComingSoon.jsx
+++ src/tempComponent/ComingSoon.jsx
@@ -37,7 +37,7 @@ const ComingSoon = () => {
     }));
 
     useEffect(() => {
-        const launchDate = new Date('2025-11-01T00:00:00');
+        const launchDate = new Date('2025-11-03T00:00:00');
 
         const calculateTimeLeft = () => {
             const now = new Date().getTime();
